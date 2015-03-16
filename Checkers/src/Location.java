092de7093816c617a2656a80614bc6625f76e394
hemantkunda <hemantkunda@gmail.com>
2015-03-16 09:22:54 -0700

    Update Location.java

--- Checkers/src/Location.java
+++ Checkers/src/Location.java
@@ -23,45 +23,55 @@ public class Location
 		y = c;
 	}
 	
+	// Returns the x coordinate of the Location.
 	public int x()
 	{
 		return x;
 	}
 	
+	// Returns the y coordinate of the Location.
 	public int y()
 	{
 		return y;
 	}
 	
+	// Returns a Location object that is visually located one space down and one space to the left of this Location.
 	public Location downLeft()
 	{
 		return new Location(x+1, y-1);
+		
 	}
-	
+	// Returns a Location object that is visually located one space down and one space to the right of this Location.
 	public Location downRight()
 	{
 		return new Location(x+1, y+1);
 	}
 	
+	// Returns a Location object that is visually located one space up and one space to the left of this Location.
 	public Location upLeft()
 	{
 		return new Location(x-1,y-1);
 	}
 	
+	// Returns a Location object that is visually located one space up and one space to the right of this Location.
 	public Location upRight()
 	{
 		return new Location(x-1, y+1);
 	}
 	
+	// Returns true if this Location and other have the same x and y coordinates.
 	public boolean equals(Location other)
 	{
 		return (other.x() == x() && other.y() == y());
 	}
 	
+	// Returns a Location object located between this Location and the specified Location by averaging the x and y coordinates.
 	public Location between(Location l)
 	{
 		return new Location((this.x() + l.x())/2, (this.y() + l.y())/2);
 	}
+	
+	// Returns a String containing the Location's information
 	public String toString()
 	{
 		return "Location(" + x() + ", " + y() + ")" ;
